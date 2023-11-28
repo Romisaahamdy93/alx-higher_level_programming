3d04984040ac9aedbb646e37638b44227dec56b4
Romisaa <romisaa.hamdy@yahoo.com>
2023-11-28 13:33:38 -0800

    printdigit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        number = number * -1
+        digit = (number * -1) % 10
+    elif number >= 0:
         digit = number % 10
-        print("{:d}".format(digit), end="")
-        return digit
+    print("{:d}".format(digit), end="")
+    return digit
