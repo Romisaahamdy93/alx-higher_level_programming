c2772f855a969fe4154f8513de9524d1290922a4
Romisaa <romisaa.hamdy@yahoo.com>
2023-11-28 11:27:10 -0800

    print

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 99):
     if number == 99:
-        print("{:02d}".format(i), end="\n")
+        print("{}".format(i), end="\n")
     else:
         print("{:02d}".format(i), end=", ")
