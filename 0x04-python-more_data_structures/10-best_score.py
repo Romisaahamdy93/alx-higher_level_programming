ef429ed2383faef009f6bdf9acc5f48d86e1fbc5
Romisaa <romisaa.hamdy@yahoo.com>
2023-12-06 14:27:35 -0800

    mul

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a-dictionary is None:
+        return None
+    bigscore = 0
+    bigkey = None
+    for kو s in a_dictionary.items():
+        if s > bigscore:
+            bigscore = s
+            bigkey = k
+    return bigkey
