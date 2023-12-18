2ca128d274911bc91e63c02f6a203e269a521e85
romisaa <romisaa.hamdy@yahoo.com>
2023-12-18 11:32:35 -0800

    edit

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -9,4 +9,4 @@ def safe_function(fct, *args):
     except IndexError:
         result = None
         sys.stderr.write("Exception: list index out of range\n")
-    return result
+    return (result)
