127fd906079f955ddc17ad927157e0a4f19c35ae
romisaa <romisaa.hamdy@yahoo.com>
2023-12-18 08:39:11 -0800

    0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    try:
+        for i in my_list:
+            if i < x:
+                print("{}".format(my_list[i], end="")
+                i = i + 1
+        print()
+    except IndexError:
+        pass
+    finally:
+        return idx
