e66f65ba192b270b9649ce187508238cf8a1c3a0
romisaa <romisaa.hamdy@yahoo.com>
2023-12-18 09:55:37 -0800

    edit

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,5 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             y += 1
             continue
-        print("")
-        return (i)
+    print("")
+    return (i)
