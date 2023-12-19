23377d4943e165a098aeba0d95ade588dde84c5f
romisaa <romisaa.hamdy@yahoo.com>
2023-12-19 03:46:58 -0800

    edit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square module."""
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Initialization a new Square.
+
+        Args:
+            size: size of Square.
+        """
+        self.__size = size
