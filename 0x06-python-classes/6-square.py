3e5e48edb1923ee503866ca1201cb6f3f66aad01
romisaa <romisaa.hamdy@yahoo.com>
2023-12-19 13:20:23 -0800

    edit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,12 +12,12 @@ class Square:
             size: size of Square.
             position: position of Square.
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
-        @property
+    @property
     def size(self):
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
@@ -30,7 +30,7 @@ class Square:
 
     @property
     def position(self):
-        return self.__position
+        return (self.__position)
 
     @position.setter
     def position(self, value):
@@ -42,7 +42,7 @@ class Square:
             self.__position = value
 
     def area(self):
-        return self.__size ** 2
+        return (self.__size * self.__size)
 
     def my_print(self):
         if self.__size == 0:
