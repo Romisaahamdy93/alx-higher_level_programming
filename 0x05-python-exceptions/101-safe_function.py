6bb4944d555ecca4ce79a45b2eb233ba5f32df59
romisaa <romisaa.hamdy@yahoo.com>
2023-12-19 03:16:47 -0800

    edit

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-import sys
 def safe_function(fct, *args):
     try:
         result = fct(*args)
-    except ZeroDivisionError:
-        result = None
-        sys.stderr.write("Exception: division by zero\n")
-    except IndexError:
-        result = None
-        sys.stderr.write("Exception: list index out of range\n")
-    return (result)
+        return result
+    except Exception as error:
+        import sys
+        print("Exception:", error, file=sys.stderr)
+        return None
