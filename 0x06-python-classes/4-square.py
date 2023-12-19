e5b179ab491aede76630c306cad1456318c09daf
romisaa <romisaa.hamdy@yahoo.com>
2023-12-19 04:27:35 -0800

    edit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,12 +13,10 @@ class Square:
         """
         self.__size = size
 
-
     @property
     def size(self):
         """Property for current size of square."""
-        return self.__size
-
+        return (self.__size)
 
     @size.setter
     def size(self, value):
@@ -27,7 +25,6 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
 
     def area(self):
         """Area of square.
