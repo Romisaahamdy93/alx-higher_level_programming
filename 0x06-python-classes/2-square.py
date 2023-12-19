9aced49149053a801bd064ef1b96f6e10ec165c5
romisaa <romisaa.hamdy@yahoo.com>
2023-12-19 03:58:15 -0800

    edit

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Initialization a new Square.
+
+        Args:
+            size: size of Square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
