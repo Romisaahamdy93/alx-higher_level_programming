3b02e96dcb32b320252b2371e162b9af1ce5b7e3
Romisaa <romisaa.hamdy@yahoo.com>
2024-01-04 10:49:47 -0800

    1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,21 +4,14 @@ class Rectangle:
     """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Initialize a rectangle.
-
-        Args:
-            width (int): The width of rectangle.
-            height (int): The height of rectangle.
-        """
+        """Initialize a rectangle."""
         self.width = width
         self.height = height
-
 
     @property
     def width(self):
         """Get the width of rectangle."""
         return self.__width
-
 
     @width.setter
     def width(self, value):
@@ -32,7 +25,6 @@ class Rectangle:
     def height(self):
         """Get the height of rectangle."""
         return self.__height
-
 
     @height.setter
     def height(self, value):
