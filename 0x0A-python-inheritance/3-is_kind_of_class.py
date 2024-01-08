c9a2cb359cedf51ec7c0de99c02df937bc675113
Romisaa <romisaa.hamdy@yahoo.com>
2024-01-08 11:45:10 -0800

    3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for is_kind_of_class method."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True if object isinstance,if object isinstance of class inherited."""
+    return isinstance(obj, a_class)
