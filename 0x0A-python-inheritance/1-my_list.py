c334f4a1c3f1c571215c844693d96fef2b00c5b8
Romisaa <romisaa.hamdy@yahoo.com>
2024-01-08 11:24:24 -0800

    0

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for MyList class."""
+
+class Mylist(list):
+    """A subclass."""
+    def __init__(self):
+        """Initialize object."""
+        super().__init__()
+
+
+    def print_sorted(self):
+        """Prints the list, but sorted."""
+        print(sorted(self))
