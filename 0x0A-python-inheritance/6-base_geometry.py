b5148470f1ee95fddfd73d118b34c795ff8b0861
Romisaa <romisaa.hamdy@yahoo.com>
2024-01-08 11:58:27 -0800

    tasks

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class."""
+
+
+class BaseGeometry:
+    """ABaseGeometry class."""
+    def area(self):
+        """Raises an Exception with the message."""
+        raise Exception("area() is not implemented")
