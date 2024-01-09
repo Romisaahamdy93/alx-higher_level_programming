f1b2bbb126e269ebd783ecbd4e3d8b538db99173
Romisaa <romisaa.hamdy@yahoo.com>
2024-01-08 16:37:45 -0800

    edit

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,50 +1,20 @@
 #!/usr/bin/python3
-"""Module to define a Square class."""
-
-
-class BaseGeometry:
-    """Class representing a base geometry."""
-
-    def area(self):
-        """Method to calculate the area."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Method to validate 'value' parameter."""
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """Class representing a rectangle."""
-
-    def __init__(self, width, height):
-        """Initialize a rectangle instance."""
-        self.__width = width
-        self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-
-    def area(self):
-        """Method to compute the area of the rectangle."""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Return the rectangle description."""
-        return f"[Rectangle] {self.__width}/{self.__height}"
+'''Module for Rectangle class.'''
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Class representing a square."""
-
+    '''A subclass representing a rectangle.'''
     def __init__(self, size):
-        """Initialize a square instance."""
-        super().__init__(size, size)
-        self.__size = size
+        '''Constructor.'''
         self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        '''Method for area of square.'''
+        return self.__size ** 2
 
     def __str__(self):
-        """Return the square description."""
-        return f"[Square] {self.__size}/{self.__size}"
+        '''Returns string representation of this square.'''
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
