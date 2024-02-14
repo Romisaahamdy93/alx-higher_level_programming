3e648ff0539743d3f757bbe53b24ce1519ac0c43
Romisaahamdy93 <romisaa.hamdy@yahoo.com>
2024-02-14 13:37:40 -0800

    102

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT tv_shows.title, SUM(rating.rate) as rating_sum
+SELECT title, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_shows
-LEFT JOIN rating ON tv_shows.id = rating.show_id
-GROUP BY tv_shows.id
-ORDER BY rating_sum DESC, tv_shows.title ASC;
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC;
