62650f63112c559418da54c33a9b0f062ef8d187
Romisaahamdy93 <romisaa.hamdy@yahoo.com>
2024-02-14 12:33:24 -0800

    tasks

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -4,5 +4,5 @@ FROM tv_genres
 LEFT JOIN show_genres ON tv_genres.id = show_genres.genre_id
 LEFT JOIN tv_shows ON show_genres.show_id = tv_shows.id
 LEFT JOIN rating ON tv_shows.id = rating.show_id
-GROUP BY tv_genres.id
+GROUP BY tv_genres.name
 ORDER BY rating_sum DESC, tv_genres.name ASC;
