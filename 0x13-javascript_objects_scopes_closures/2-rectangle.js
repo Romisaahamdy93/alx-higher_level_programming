a35737aa3755b3398bce631794eff18c8439a019
Romisaa <romisaa.hamdy@yahoo.com>
2024-03-12 15:32:11 -0700

    3

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      // If w or h is equal to 0 or not a positive integer, create an empty object
-      return {};
-    } else {
-      // Otherwise, initialize the instance attributes width and height
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
