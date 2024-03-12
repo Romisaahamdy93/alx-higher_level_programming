28fecc8a148b5420e1981948098765c59ea5e4a7
Romisaa <romisaa.hamdy@yahoo.com>
2024-03-12 15:51:33 -0700

    10

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-	const reversedList = [];
-	for (let i = list.length - 1; i >= 0; i--) {
-		reversedList.push(list[i]);
-	}
-	return reversedList;
+  let len = list.length - 1;
+  let i = 0;
+  while ((len - i) > 0) {
+    const aux = list[len];
+    list[len] = list[i];
+    list[i] = aux;
+    i++;
+    len--;
+  }
+  return list;
 };
