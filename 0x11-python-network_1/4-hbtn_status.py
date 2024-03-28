c5b5ac32a610483be4123e9161adf8ff9f503e06
Romisaa <romisaa.hamdy@yahoo.com>
2024-03-28 14:49:25 -0700

    2

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A Python script that fetches
+-https://alx-intranet.hbtn.io/status using the requests package.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    data = response.text
+
+    print("Body response:")
+    print("\t- type:", type(data))
+    print("\t- content:", data)
